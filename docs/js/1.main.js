95075354af88320ed341c8b69067f63e42eef64c
antwan <antwanvdm@gmail.com>
2020-09-26 15:07:15 +0200

    Update the packages to recent secure versions

--- docs/js/1.main.js
+++ docs/js/1.main.js
@@ -7,7 +7,7 @@
 /*! exports provided: title, introQuestion, inputName, inputSubmit, loading, guessPokemon, wrongAnswer, goodAnswer, endMessage, default */
 /***/ (function(module) {
 
-module.exports = {"title":"Pokémon: Raad ze allemaal!","introQuestion":"Hoeveel Pokémon uit gen I, II & III herken jij in één oogopslag?","inputName":"jouw naam","inputSubmit":"Ik raad ze allemaal! &#x1F525;","loading":"Pokémons worden momenteel ingeladen","guessPokemon":"Wie is deze Pokémon?!","wrongAnswer":"Helaas! Deze Pokémon was [[pokemon]]! Nieuwe Pokémon is onderweg!","goodAnswer":"Veel beter gaat het niet worden! Nieuwe Pokémon is onderweg!","endMessage":"Dank voor het spelen, Je eindscore is: [[score]] / [[maxScore]]"};
+module.exports = JSON.parse("{\"title\":\"Pokémon: Raad ze allemaal!\",\"introQuestion\":\"Hoeveel Pokémon uit gen I, II & III herken jij in één oogopslag?\",\"inputName\":\"jouw naam\",\"inputSubmit\":\"Ik raad ze allemaal! &#x1F525;\",\"loading\":\"Pokémons worden momenteel ingeladen\",\"guessPokemon\":\"Wie is deze Pokémon?!\",\"wrongAnswer\":\"Helaas! Deze Pokémon was [[pokemon]]! Nieuwe Pokémon is onderweg!\",\"goodAnswer\":\"Veel beter gaat het niet worden! Nieuwe Pokémon is onderweg!\",\"endMessage\":\"Dank voor het spelen, Je eindscore is: [[score]] / [[maxScore]]\"}");
 
 /***/ })
 
